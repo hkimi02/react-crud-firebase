8c8f34a0a380c515e3dabb3becb4fd768270c63c
hkimi amin <hkimiamin02@gmail.com>
2023-06-15 17:26:42 +0100

    add delete user with confirm

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { db } from '../firebase-config'
-import { collection, getDocs } from 'firebase/firestore'
+import { deleteDoc, collection, getDocs, doc } from 'firebase/firestore'
 import {
   CardActionArea,
   CardActions,
@@ -13,9 +13,16 @@ import { Card } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Button from '@mui/material/Button'
 import CreateIcon from '@mui/icons-material/Create'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
 function Home() {
   const [users, setUsers] = useState([])
   const UserCollectionRef = collection(db, 'users')
+  const [RefDelete, setRefDelete] = useState(null)
+  const [open, setOpen] = useState(false)
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(UserCollectionRef)
@@ -24,6 +31,24 @@ function Home() {
     }
     getUsers()
   }, [UserCollectionRef])
+
+  const handleClickOpen = (Refdelete) => {
+    setOpen(true)
+    setRefDelete(Refdelete)
+    console.log('hello')
+    console.log(Refdelete)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const handleDelete = async () => {
+    //await db.collection('users').doc(RefDelete).delete()
+    console.log(RefDelete)
+    if (RefDelete) {
+      await deleteDoc(doc(db, 'users', RefDelete))
+      setOpen(false)
+    }
+  }
   return (
     <div>
       <h1>list persons</h1>
@@ -31,7 +56,7 @@ function Home() {
         <div>
           <Grid container>
             {users.map((user) => (
-              <Grid item xs={4} key={user.name}>
+              <Grid item xs={4} key={user.id}>
                 <Card
                   variant="outlined"
                   sx={{ maxWidth: 345, margin: '10px', padding: '10px' }}
@@ -48,7 +73,11 @@ function Home() {
                     <h4>age : {user.age}</h4>
                   </CardContent>
                   <CardActions>
-                    <Button variant="text" sx={{ color: 'red' }}>
+                    <Button
+                      variant="text"
+                      sx={{ color: 'red' }}
+                      onClick={() => handleClickOpen(user.id)}
+                    >
                       <DeleteIcon />
                     </Button>
                     <Button variant="text" sx={{ color: 'orange' }}>
@@ -61,6 +90,27 @@ function Home() {
           </Grid>
         </div>
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {'confirm dialog to delete person'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            are you sure you want to delete this person ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleDelete} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
